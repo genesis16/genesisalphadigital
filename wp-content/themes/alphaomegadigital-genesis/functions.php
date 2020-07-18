a238868783859d5360b07d4bf251daf6df571a1d
rdoRp9HioP <heny.jon112@gmail.com>
2020-07-19 09:49:59 +1000

    modified theme `alphaomegadigital genesis` version 1.0

--- wp-content/themes/alphaomegadigital-genesis/functions.php
+++ wp-content/themes/alphaomegadigital-genesis/functions.php
@@ -261,3 +261,4 @@ add_theme_support( 'custom-logo', array(
 //* Force full-width-content layout setting
 add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
 
+
