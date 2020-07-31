7b221af8243ca0152c9875fd176e8a6a6f8b10a6
rdoRp9HioP <heny.jon112@gmail.com>
2020-07-31 15:42:46 +1000

    updated plugin `Genesis Portfolio Pro` version 1.2.2

---
+++ wp-content/plugins/genesis-portfolio-pro/plugin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Main Plugin File
+ *
+ * @package genesis-portfolio-pro
+ */
+
+/**
+ * Plugin Name: Genesis Portfolio Pro
+ * Plugin URI: https://wordpress.org/plugins/genesis-portfolio-pro/
+ * Description: Adds default portfolio to any Genesis HTML5 theme.
+ * Version: 1.2.2
+ * Author: StudioPress
+ * Author URI: https://www.studiopress.com
+ * Text Domain: genesis-portfolio-pro
+ * Domain Path: /languages
+ */
+
+require_once plugin_dir_path( __FILE__ ) . 'genesis-portfolio-pro.php';
