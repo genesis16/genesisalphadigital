6e19e260cfd276da426da97c9d6972d17cffa150
genesis16 <janeleeshian@gmail.com>
2020-08-31 16:42:17 +1000

    Backed up the database with Revisr.

--- wp-content/uploads/revisr-backups/revisr_wpah_commentmeta.sql
+++ wp-content/uploads/revisr-backups/revisr_wpah_commentmeta.sql
@@ -25,7 +25,6 @@ CREATE TABLE `wpah_commentmeta` (
 
 LOCK TABLES `wpah_commentmeta` WRITE;
 /*!40000 ALTER TABLE `wpah_commentmeta` DISABLE KEYS */;
-INSERT INTO `wpah_commentmeta` VALUES (1,12,'_wxr_import_user','1'),(2,25,'_wxr_import_user','1'),(3,4298,'akismet_result','false'),(4,4298,'akismet_history','a:4:{s:4:\"time\";s:15:\"1372292563.4895\";s:7:\"message\";s:28:\"Akismet cleared this comment\";s:5:\"event\";s:9:\"check-ham\";s:4:\"user\";s:0:\"\";}'),(5,4298,'akismet_history','a:4:{s:4:\"time\";s:15:\"1372292592.5121\";s:7:\"message\";s:47:\"bgardner changed the comment status to approved\";s:5:\"event\";s:15:\"status-approved\";s:4:\"user\";s:8:\"bgardner\";}'),(6,4299,'akismet_result','false'),(7,4299,'akismet_history','a:4:{s:4:\"time\";s:15:\"1372292858.5887\";s:7:\"message\";s:28:\"Akismet cleared this comment\";s:5:\"event\";s:9:\"check-ham\";s:4:\"user\";s:0:\"\";}'),(8,4299,'akismet_history','a:4:{s:4:\"time\";s:14:\"1372292865.222\";s:7:\"message\";s:47:\"bgardner changed the comment status to approved\";s:5:\"event\";s:15:\"status-approved\";s:4:\"user\";s:8:\"bgardner\";}');
 /*!40000 ALTER TABLE `wpah_commentmeta` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
