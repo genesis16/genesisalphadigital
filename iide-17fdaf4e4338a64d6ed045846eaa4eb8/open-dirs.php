f05bf58cbe51a0cde17dccc264954c3126af80bf
genesis16 <janeleeshian@gmail.com>
2020-09-09 15:57:09 +1000

    Daily backup - September 9, 2020

--- iide-17fdaf4e4338a64d6ed045846eaa4eb8/open-dirs.php
+++ iide-17fdaf4e4338a64d6ed045846eaa4eb8/open-dirs.php
@@ -1,3 +1,3 @@
 <?php
 // Define the current open directories.
-define( 'IIDE_OPEN_DIRS', 'wp-content,wp-content/themes,wp-content/themes/alpha-omega-digital/scss,wp-content/themes/alpha-omega-digital/scss/partials,wp-content/themes/studio-pro,wp-content/themes/studio-pro/assets,wp-content/themes/studio-pro/assets/styles' );
+define( 'IIDE_OPEN_DIRS', 'wp-content/themes,wp-content,wp-content/themes/monochrome-pro,wp-content/themes/monochrome-pro/lib,wp-content/themes/monochrome-pro/lib/gutenberg' );
--- iide-17fdaf4e4338a64d6ed045846eaa4eb8/opened-dirs.php
+++ iide-17fdaf4e4338a64d6ed045846eaa4eb8/opened-dirs.php
@@ -1,3 +1,3 @@
 <?php
 // Define the current opened directories.
-define( 'IIDE_OPENED_DIRS', 'wp-content,wp-content/themes,wp-content/themes/alpha-omega-digital,wp-content/themes/alpha-omega-digital/scss,wp-content/themes/alpha-omega-digital/scss/partials,wp-content/themes/studio-pro,wp-content/themes/studio-pro/assets,wp-content/themes/studio-pro/assets/styles' );
+define( 'IIDE_OPENED_DIRS', 'wp-content/themes,wp-content,wp-content/themes/monochrome-pro,wp-content/themes/monochrome-pro/lib,wp-content/themes/monochrome-pro/lib/gutenberg' );
